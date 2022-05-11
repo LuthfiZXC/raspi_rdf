af3fc33023cc570e754b25dec374b1ca1173e370
LuthfiZXC <59855454+LuthfiZXC@users.noreply.github.com>
2022-05-11 10:15:02 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,12 @@ sudo apt install -y gnuradio gr-osmosdr python-pip python2-dev
 #installing required pip modules
 sudo pip install wheel ephem pyasn1 pyasn1-modules service_identity twisted txws six pyserial pathlib
 
+#installing shinysdr
+cd /home/pi
+sudo git clone https://github.com/kpreid/shinysdr/
+cd shinysdr
+sudo python2 setup.py build
+sudo python2 setup.py install
+sudo shinysdr --create websdr
 
+# sudo sed -i "s/\'osmo\'\, OsmoSDRDevice\(\'\'\)/\'osmo\'\, OsmoSDRDevice\(\'rtl\-sdr\'\)/" websdr/config.py
