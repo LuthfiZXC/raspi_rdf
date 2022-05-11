24093b0e384c43c6aa43ee06663efd1cf66c314c
LuthfiZXC <59855454+LuthfiZXC@users.noreply.github.com>
2022-05-11 09:30:08 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#installing required packages
+sudo apt install -y gnuradio gr-osmosdr python-pip python2-dev
+
+#installing required pip modules
+sudo pip install wheel ephem pyasn1 pyasn1-modules service_identity twisted txws six pyserial pathlib
+
+
